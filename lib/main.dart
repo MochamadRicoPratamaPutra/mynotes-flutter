5eb0429042df6700258f37ec8aba609374ffaf7f
Mochamad Rico Pratama Putra <mricopratamaputra@gmail.com>
2024-12-12 10:56:24 +0700

    Add Login View to Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mynotes/views/login_view.dart';
 import 'firebase_options.dart';
 
 void main() {
@@ -12,7 +13,7 @@ void main() {
       useMaterial3: true,
     ),
     // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    home: const RegisterView(),
+    home: const LoginView(),
   ));
 }
 
