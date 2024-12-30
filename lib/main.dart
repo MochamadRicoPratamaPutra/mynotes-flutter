e8e9fe8a75489f256c4a27c086b17949ccffcc1b
Mochamad Rico Pratama Putra <mricopratamaputra@gmail.com>
2024-12-30 14:10:18 +0700

    Assign Verify Email Widget to its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
@@ -44,35 +43,6 @@ class HomePage extends StatelessWidget {
             return const CircularProgressIndicator();
         }
       },
-    );
-  }
-}
-
-class VerifyEmaiLView extends StatefulWidget {
-  const VerifyEmaiLView({super.key});
-
-  @override
-  State<VerifyEmaiLView> createState() => _VerifyEmaiLViewState();
-}
-
-class _VerifyEmaiLViewState extends State<VerifyEmaiLView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Verify Email'),
-      ),
-      body: Column(
-        children: [
-          const Text('Please verify your email address:'),
-          TextButton(
-              onPressed: () async {
-                final user = FirebaseAuth.instance.currentUser;
-                await user?.sendEmailVerification();
-              },
-              child: const Text("Send Email Verification"))
-        ],
-      ),
     );
   }
 }
