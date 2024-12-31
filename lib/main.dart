18b71f60bc861c800ee184615bf30ab4c04f16c4
Mochamad Rico Pratama Putra <mricopratamaputra@gmail.com>
2024-12-31 09:44:21 +0700

    Base Notes View

--- lib/main.dart
+++ lib/main.dart
@@ -37,18 +37,36 @@ class HomePage extends StatelessWidget {
             final user = FirebaseAuth.instance.currentUser;
             if (user != null) {
               if (user.emailVerified) {
-                print('Email is verified');
+                return const NotesView();
               } else {
                 return const VerifyEmaiLView();
               }
             } else {
               return const LoginView();
             }
-            return const Text('Done');
           default:
             return const CircularProgressIndicator();
         }
       },
+    );
+  }
+}
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+      ),
+      body: const Text('Hello World'),
     );
   }
 }
