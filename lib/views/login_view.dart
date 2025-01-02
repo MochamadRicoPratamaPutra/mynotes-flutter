986950733068b225183180ecc8390d38db9b3321
Mochamad Rico Pratama Putra <mricopratamaputra@gmail.com>
2025-01-02 10:21:19 +0700

    Add Route and Navigator from Login to Notes

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -56,7 +56,10 @@ class _LoginViewState extends State<LoginView> {
                   final userCredential = await FirebaseAuth.instance
                       .signInWithEmailAndPassword(
                           email: email, password: password);
-                  devtools.log(userCredential.toString());
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/notes/',
+                    (route) => false,
+                  );
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'user-not-found') {
                     devtools.log('User not found');
