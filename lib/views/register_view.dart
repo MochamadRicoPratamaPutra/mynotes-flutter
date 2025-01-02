0cdf2f04e8d83f9a6db6ba29d19f5400e67c8680
Mochamad Rico Pratama Putra <mricopratamaputra@gmail.com>
2025-01-02 13:32:28 +0700

    Add Error Handling for Register

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -58,17 +57,33 @@ class _RegisterViewState extends State<RegisterView> {
                   final userCredential = await FirebaseAuth.instance
                       .createUserWithEmailAndPassword(
                           email: email, password: password);
-                  devtools.log(userCredential.toString());
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'weak-password') {
-                    devtools.log('Weak Password');
+                    await showErrorDialog(
+                      context,
+                      'Weak Password',
+                    );
                   } else if (e.code == 'email-already-in-use') {
-                    devtools.log("Email is already in use");
+                    await showErrorDialog(
+                      context,
+                      'Email is already in use',
+                    );
                   } else if (e.code == 'invalid-email') {
-                    devtools.log('Invalid Email');
+                    await showErrorDialog(
+                      context,
+                      'This is an invalid email address',
+                    );
                   } else {
-                    devtools.log(e.code);
+                    await showErrorDialog(
+                      context,
+                      'Error: ${e.code}',
+                    );
                   }
+                } catch (e) {
+                  await showErrorDialog(
+                    context,
+                    'Error: ${e.toString()}',
+                  );
                 }
               },
               child: const Text('Register')),
