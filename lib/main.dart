31dd227625992282db21b09316be58a19e2d7ae0
Mochamad Rico Pratama Putra <mricopratamaputra@gmail.com>
2025-01-13 14:18:30 +0700

    Change New Note View Name and Add Base Stateful Widget for New Note View

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/notes/new_note_view.dart';
 import 'package:mynotes/views/notes/notes_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
@@ -17,6 +18,7 @@ void main() {
       registerRoute: (context) => const RegisterView(),
       notesRoute: (context) => const NotesView(),
       verifyEmailRoute: (context) => const VerifyEmaiLView(),
+      newNoteRoute: (context) => const NewNoteView(),
     },
   ));
 }
