9a653ada94a9004f65d3ad8547a003156d67e395
Mochamad Rico Pratama Putra <mricopratamaputra@gmail.com>
2025-01-24 13:44:12 +0700

    Add Title in Register View and Adjust Positioning Widget in Register View

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -66,7 +66,9 @@ class _RegisterViewState extends State<RegisterView> {
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              const Text('Enter your email and password to see your notes'),
               TextField(
                 controller: _email,
                 autocorrect: false,
@@ -81,21 +83,27 @@ class _RegisterViewState extends State<RegisterView> {
                 decoration:
                     const InputDecoration(hintText: 'Enter your password'),
               ),
-              TextButton(
-                  onPressed: () async {
-                    final email = _email.text;
-                    final password = _password.text;
-                    context.read<AuthBloc>().add(AuthEventRegister(
-                          email,
-                          password,
-                        ));
-                  },
-                  child: const Text('Register')),
-              TextButton(
-                  onPressed: () {
-                    context.read<AuthBloc>().add(const AuthEventLogOut());
-                  },
-                  child: const Text('Already registered? Login here!'))
+              Center(
+                child: Column(
+                  children: [
+                    TextButton(
+                        onPressed: () async {
+                          final email = _email.text;
+                          final password = _password.text;
+                          context.read<AuthBloc>().add(AuthEventRegister(
+                                email,
+                                password,
+                              ));
+                        },
+                        child: const Text('Register')),
+                    TextButton(
+                        onPressed: () {
+                          context.read<AuthBloc>().add(const AuthEventLogOut());
+                        },
+                        child: const Text('Already registered? Login here!'))
+                  ],
+                ),
+              ),
             ],
           ),
         ),
