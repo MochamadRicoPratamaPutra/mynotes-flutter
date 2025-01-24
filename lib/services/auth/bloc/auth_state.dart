7df70f83d5dac1cb891d611f100c42a40d64108e
Mochamad Rico Pratama Putra <mricopratamaputra@gmail.com>
2025-01-24 11:05:54 +0700

    Make exception for AuthStateForgotPassword nullable

--- lib/services/auth/bloc/auth_state.dart
+++ lib/services/auth/bloc/auth_state.dart
@@ -25,7 +25,7 @@ class AuthStateRegistering extends AuthState {
 }
 
 class AuthStateForgotPassword extends AuthState {
-  final Exception exception;
+  final Exception? exception;
   final bool hasSentEmail;
 
   const AuthStateForgotPassword({
